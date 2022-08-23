acf7360c98debbdc435ca1c2928e31dcc1b4591d
Student astrum <student@astrum.domain>
2022-08-23 09:42:50 +0500

    my yelp with rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,42 @@
 class HomeController < ApplicationController
+  before_action :set_restaurant, only: %i[ show edit update destroy ]
   def page
+    @restaurants = Restaurant.all
+    @restaurant = Restaurant.new
+    # if !user_signed_in?
+    #   redirect_to new_user_session_path
+    # end
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants or /restaurants.json
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to root_path, notice: "Restaurant was successfully created." }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :city)
   end
 end
